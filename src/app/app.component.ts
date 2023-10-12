26632a7f00fe02ff24cc3ddae8d706c0d68d1040
dkolyvas <kolyvas@gmail.com>
2023-10-12 20:40:21 +0300

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonComponent } from './person/person.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PersonComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   name: string = "Dimitris";
   surname:string = "Kolyvas";
+
 }
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-person',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent {
+  person = {
+    givenName :"Dimitrios",
+    surName: "Kolyvas",
+    age: 0x2a, 
+    email: "kolyvas@gmail.com"
+  };
+
+}
