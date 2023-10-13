dc3084a0c92b93fa6f915131524446fe3ed3e165
dkolyvas <kolyvas@gmail.com>
2023-10-13 18:57:22 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonComponent } from './person/person.component';
+import { Person } from './intefaces/person';
+import { PersonAltComponent } from './person-alt/person-alt.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PersonComponent],
+  imports: [CommonModule, PersonComponent, PersonAltComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -14,5 +16,13 @@ import { PersonComponent } from './person/person.component';
 export class AppComponent {
   name: string = "Dimitris";
   surname:string = "Kolyvas";
+
+  me:Person = {
+    givenName: "Dimitris",
+    surname: "Kolyvas",
+    age: 38,
+    email: "kolyvas@gmail.com",
+    address: "Spetson 106 Athens"
+  };
 
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Person } from '../intefaces/person';
 
 @Component({
   selector: 'app-person',
@@ -9,11 +10,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent {
-  person = {
-    givenName :"Dimitrios",
-    surName: "Kolyvas",
-    age: 0x2a, 
-    email: "kolyvas@gmail.com"
-  };
+ // person :Person ={
+   // givenName: "Dimitrios",
+    // surname: "Kolyvas",
+    // age: 38,
+    // email: "kolyvas@gmail.com",
+    // address: "Spetson 106 Athesn"
+  //}
+ @Input() person:Person={
+  givenName: "first name",
+  surname :"surname",
+  age: 0,
+  email: "email",
+  address: "address"
+ };
+
 
 }
