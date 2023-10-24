f09e2c6c9be0709fc3b81cd7960fac7afad61683
dkolyvas <kolyvas@gmail.com>
2023-10-24 20:06:44 +0300

    first commit

--- src/app/crud-demo/crud/delete-user/delete-user.component.ts
+++ src/app/crud-demo/crud/delete-user/delete-user.component.ts
@@ -2,15 +2,17 @@ import { Component, ElementRef, EventEmitter, Inject, Output, ViewChild } from '
 import { CommonModule } from '@angular/common';
 import { Person } from 'src/app/intefaces/person';
 import { AppService } from 'src/app/app.service';
-import { PersonCardComponent } from "../../person-card/person-card.component";
+import { PersonCardComponent } from "../../../person-card/person-card.component";
 import { HttpClient } from '@angular/common/http';
+import { CrudUserSearchComponent } from "../../utils/crud-user-search/crud-user-search.component";
+import { DangerAreYouSureComponent } from "../../utils/danger-are-you-sure/danger-are-you-sure.component";
 
 @Component({
     selector: 'app-delete-user',
     standalone: true,
     templateUrl: './delete-user.component.html',
     styleUrls: ['./delete-user.component.css'],
-    imports: [CommonModule, PersonCardComponent]
+    imports: [CommonModule, PersonCardComponent, CrudUserSearchComponent, DangerAreYouSureComponent]
 })
 export class DeleteUserComponent {
   foundUser: Person|undefined
@@ -19,9 +21,19 @@ export class DeleteUserComponent {
   @Output() userDeleted = new EventEmitter()
   constructor(private appService:AppService = Inject(AppService)){}
 
-  onClick(){
-    const id = this.userIdInput.nativeElement.value
-    this.appService.deleteUser(parseInt(id)).subscribe({
+  onConfirm(confirm:boolean){
+    if(confirm && this.foundUser){
+      const id = this.foundUser.id ??-1;
+      this.delete(id);
+    }
+    else{
+      this.foundUser = undefined;
+    }
+  }
+
+  delete(id:number){
+    
+    this.appService.deleteUser(id).subscribe({
       next:(user)=>{
         console.log(user)
         this.userDeleted.emit()
@@ -34,5 +46,11 @@ export class DeleteUserComponent {
       complete:()=>{}
     }
     )
+  }
+
+  onUserFound(user:Person|undefined){
+    if(user){
+      this.foundUser = user
+    }
   }
 }
